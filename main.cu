9ac164606ac257665517eeaa3c33f205fd112e00
Diego Piccinotti <elimdor@live.it>
2019-03-19 21:08:06 +0100

    Partial work on pagerank update

--- main.cu
+++ main.cu
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <stdio.h>
-#include "Matrix.h"
 #include <fstream>
 #include <sstream>
-#include <vector>
 //#include <cub/cub.cuh>
 //#include "Utilities.cuh"
 
@@ -14,7 +12,7 @@ using namespace std;
 
 //const int N = 256;
 
-
+// Unoptimized kernel
 __global__ void addReduceKernel(double *inData, double *outData){
 	extern __shared__ double sharedData[];
 
@@ -35,28 +33,29 @@ __global__ void addReduceKernel(double *inData, double *outData){
 	if(tid == 0) outData[blockIdx.x] = sharedData[0];
 }
 
+// Default reduction kernel from seminar slides - check for possible optimizations
 template <unsigned int blockSize> __global__ void cuda_reduction(double *array_in, double *reduct, size_t array_len) {
-	  extern volatile __shared__ double sdata[];
-	  size_t  tid = threadIdx.x, gridSize = blockSize * gridDim.x, i = blockIdx.x * blockSize + tid;
-	  sdata[tid] = 0;
-	  while (i < array_len) {
+	extern volatile __shared__ double sdata[];
+	size_t  tid = threadIdx.x, gridSize = blockSize * gridDim.x, i = blockIdx.x * blockSize + tid;
+	sdata[tid] = 0;
+	while (i < array_len) {
 		sdata[tid] += array_in[i];
 		i += gridSize;
-	  }
-	  __syncthreads();
-	  if (blockSize >= 512) {
+	}
+	__syncthreads();
+	if (blockSize >= 512) {
 		if (tid < 256) sdata[tid] += sdata[tid + 256];
 		__syncthreads();
-	  }
-	  if (blockSize >= 256) {
+	}
+	if (blockSize >= 256) {
 		if (tid < 128) sdata[tid] += sdata[tid + 128];
 		__syncthreads();
-	  }
-	  if (blockSize >= 128) {
+	}
+	if (blockSize >= 128) {
 		if (tid <  64) sdata[tid] += sdata[tid + 64];
 		__syncthreads();
-	  }
-	  if (tid < 32) {
+	}
+	if (tid < 32) {
 		if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
 		if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
 		if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
@@ -64,36 +63,84 @@ template <unsigned int blockSize> __global__ void cuda_reduction(double *array_i
 		if (blockSize >= 4)  sdata[tid] += sdata[tid + 2];
 		if (blockSize >= 2)  sdata[tid] += sdata[tid + 1];
 	}
-	  if (tid == 0) reduct[blockIdx.x] = sdata[0];
-	}
+	if (tid == 0) reduct[blockIdx.x] = sdata[0];
+}
 
+
+// Perform first step of pagerank row by column product
+template <unsigned int blockSize> __global__ void weighted_sum_partial(double *reduct, double *pagerank, 
+	double *column, double *mat_data, size_t array_len, size_t pk_len){
+	extern volatile __shared__ double sdata[];
+	size_t  tid = threadIdx.x, gridSize = blockSize * gridDim.x, i = blockIdx.x * blockSize + tid;
+	sdata[tid] = 0;
+	while (i < array_len) {
+		sdata[tid] += mat_data[i]*pagerank[column[i]];
+		i += gridSize;
+	}
+	__syncthreads();
+	if (blockSize >= 512) {
+		if (tid < 256) sdata[tid] += sdata[tid + 256];
+		__syncthreads();
+	}
+	if (blockSize >= 256) {
+		if (tid < 128) sdata[tid] += sdata[tid + 128];
+		__syncthreads();
+	}
+	if (blockSize >= 128) {
+		if (tid <  64) sdata[tid] += sdata[tid + 64];
+		__syncthreads();
+	}
+	if (tid < 32) {
+		if (blockSize >= 64) sdata[tid] += sdata[tid + 32];
+		if (blockSize >= 32) sdata[tid] += sdata[tid + 16];
+		if (blockSize >= 16) sdata[tid] += sdata[tid + 8];
+		if (blockSize >= 8)  sdata[tid] += sdata[tid + 4];
+		if (blockSize >= 4)  sdata[tid] += sdata[tid + 2];
+		if (blockSize >= 2)  sdata[tid] += sdata[tid + 1];
+	}
+	if (tid == 0) reduct[blockIdx.x] = sdata[0];
+}
+
+template <unsigned int blockSize> __global__ void handle_multiply(double *reduct, double *pagerank,
+	double *row_indices, double *columns, double *mat_data,
+	size_t indices_len, size_t len_data, size_t array_len, size_t pk_len){
+		for (i = 0; i < indices_len-1; i++){
+			// Probably adding divergence, would it be meaningful to perform it directly into GPU?
+			if (row_indices[i] == row_indices[i+1]){
+				// Uniform reduction
+			}
+			else{
+				// "True" pagerank product
+				int index = row_indices[i];
+				int data_number = row_indices[i+1] - index;
+				// We need to allocate space for reduction, also how to select blocksize = data_number?
+				<BLOCKSIZE><data_number)BLOCKSIZE),BLOCKSIZE, BLOCKSIZE*sizeof(double)>weighted_sum_partial(reduct, pagerank
+				columns[index], mat_data[index],data_number);
+			}
+		}
+
+}
 int main(){
 
    	ifstream connFile, probFile;
    	connFile.open(CONNECTIONS);
-	vector<vector<double>> T_vector = {};
-	int *row_ptrs;
-	int *col_indices;
-	int *connections;
-	int nodes_number;
-	int rows;
-	int col_indices_number;
-	int conn_size;
+	int *row_ptrs, *col_indices, *connections;
+	int nodes_number, col_indices_number, conn_size, row_len;
 
 	cout << "Load connections" << endl;;
 	if (connFile){
 		string line, element;
 		
-		// Read rows number and allocate vector
+		// Read row_len number and allocate vector
 		getline(connFile, line);
-		rows = stoi(line);
-		nodes_number = rows-1;
-		row_ptrs = (int *) malloc(rows*sizeof(int));
+		row_len = stoi(line);
+		nodes_number = row_len-1;
+		row_ptrs = (int *) malloc(row_len*sizeof(int));
 		
-		// Store menaningful rows
+		// Store meaningful rows
 		getline(connFile, line);
 		stringstream ss(line);
-		for (int i = 0; i < rows; i++){
+		for (int i = 0; i < row_len; i++){
 			getline(ss, element, ',');
 			row_ptrs[i] = stoi(element);
 		}
@@ -131,19 +178,23 @@ int main(){
 	double cpu_sum = 0;
 	for (int i = 0; i < nodes_number; i++){
 		pr[i] = uniform_p;
-		cpu_sum += uniform_p;
+		//cpu_sum += uniform_p;
 	}
 
-	double *pk_gpu, *out;
-	cout << cpu_sum << endl;
+	double *pk_gpu, *out, *uniform_gpu, *new_pk;
+	//cout << cpu_sum << endl;
 
 	cudaMallocManaged(&pk_gpu, nodes_number*sizeof(double));
-	//cudaMalloc(&pk, nodes_number*sizeof(double));
 	cudaMallocManaged(&out, nodes_number*sizeof(double));
-	for (int i = 0; i < nodes_number; i++) pk_gpu[i] = 1;
-	//cout << "hi" << endl;
-	cudaMemcpy(pk_gpu, pr, nodes_number*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMallocManaged(&uniform_gpu, sizeof(double));
+	cudaMallocShared(&new_pk, sizeof(double)*nodes_number)
 
+	for (int i = 0; i < nodes_number; i++) pk_gpu[i] = uniform_p;
+	cudaMemcpy(pk_gpu, pr, nodes_number*sizeof(double), cudaMemcpyHostToDevice);
+	cudaMemcpy(uniform_gpu, uniform_p, sizeof(double), cudaMemcpyHostToDevice);
+
+	// Calculate constant weighted pagerank sum
+	
 	cuda_reduction <BLOCKSIZE> <<< nodes_number/BLOCKSIZE, BLOCKSIZE, BLOCKSIZE *sizeof(double)>>>(pk_gpu, out, nodes_number);
 	cuda_reduction <BLOCKSIZE> <<< 1, BLOCKSIZE, BLOCKSIZE *sizeof(double)>>>(out, pk_gpu, nodes_number);
 	//addReduceKernel<<<nodes_number/BLOCKSIZE, BLOCKSIZE, BLOCKSIZE*sizeof(double)>>>(pk_gpu, out);
@@ -154,6 +205,8 @@ int main(){
 
 
 	//cudaFree(pk);
+	cudaFree(uniform_gpu);
+	cudaFree(new_pk);
 	cudaFree(pk_gpu);
 	cudaFree(out);
 	
