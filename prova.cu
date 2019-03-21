6452acc34c595a4ab0f1fcedba68aa6423099f19
Diego Piccinotti <elimdor@live.it>
2019-03-21 14:47:14 +0100

    Test file

---
+++ prova.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+template <unsigned int blockSize> __global__ void test_final(){
+    printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
+}
+
+template <unsigned int blockSize> __global__ void test(){
+    printf("Hello from father block %d, thread %d\n", blockIdx.x, threadIdx.x);
+    test_final <2> <<<2,2>>> ();
+}
+
+int main(){
+    test <2> <<<2,2>>>();
+    return 0;
+}
